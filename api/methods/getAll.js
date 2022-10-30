acf39847b6c7a82ad58ec81b63c35c1dcfa20b69
rtof83 <zuichuan@msn.com>
2022-10-30 11:34:10 -0300

    adding pagination

--- api/methods/getAll.js
+++ api/methods/getAll.js
@@ -2,34 +2,55 @@ const { app, conn } = require('../database/conn');
 const checkUser = require('../middlewares/checkUser');
 
 const getAll = (route, model) => {
-  let query;
-
-  // custom queries
-  if (route === '/cars')
-    query = `SELECT c.id, c.name, c.model, c.price, c.km, c.image, c.desc, b.name AS brand, s.name AS store
-            FROM cars c
-            INNER JOIN brands b ON c.brandId = b.id
-            INNER JOIN stores s ON c.storeId = s.id
-            ORDER BY c.price
-            LIMIT :startFrom, :pageSize`;
-
   app.get(route, checkUser, async (req, res) => {
+    let query, result;
+
+    // custom queries
+    if (route === '/cars')
+      query = `SELECT c.id, c.name, c.model, c.price, c.km, c.image, c.desc, b.name AS brand, s.name AS store
+               FROM cars c
+               INNER JOIN brands b ON c.brandId = b.id
+               INNER JOIN stores s ON c.storeId = s.id`;
+    // custom queries
+
     try {
-      const pageSize = 3;
-      const total = await model.count();
-      const pages = Math.ceil(total / pageSize);
-      const pageNumber = !req.query.page ? 1 : req.query.page;
-      const startFrom = (pageNumber - 1) * pageSize;
+      if (req.query.page || req.query.name) {
+        let search = {};
 
-      const result = await (query ? conn.query(query, { replacements: {
-                              startFrom: startFrom,
-                              pageSize: pageSize
-                              }, type: conn.QueryTypes.SELECT })
-                            :
-                            model.findAll({ limit: pageSize, offset: startFrom }, {order: [['name']]}));
+        if (req.query.name) {
+          const Sequelize = require('sequelize');
+          const Op = Sequelize.Op;
 
-      // adding pagination to array
-      result.push({ page: parseInt(pageNumber), from: pages });
+          search.name = { [Op.substring]: req.query.name };
+        };
+
+        const pageSize = parseInt(process.env.PAGE_SIZE);
+        const total = await model.count({ where: search });
+        const pages = Math.ceil(total / pageSize);
+        const pageNumber = !req.query.page ? 1 : req.query.page;
+        const startFrom = (pageNumber - 1) * pageSize;
+
+        if (route === '/cars') query += ` WHERE c.name LIKE :search
+                                          ORDER BY c.price
+                                          LIMIT :startFrom, :pageSize`;
+
+        result = await (query ? conn.query(query, { replacements: {
+                          startFrom: startFrom,
+                          pageSize: pageSize,
+                          search: `%${req.query.name}%`
+                          }, type: conn.QueryTypes.SELECT })
+                        :
+                        model.findAll({ limit: pageSize, offset: startFrom, where: search},
+                                      { order: [['name']] },
+                                      ));
+
+        // adding pagination to array
+        if (result.length > 0)
+          result.push({ page: parseInt(pageNumber), from: pages });
+      } else {
+        // GET ALL
+        result = await (query ? conn.query(query, { type: conn.QueryTypes.SELECT }) : model.findAll());
+      };
 
       res.status(200).json(result);
     } catch (error) {
